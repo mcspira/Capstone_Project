a7cb7ae6a7452a73e020f85a582c8305c01cbee1
mcspira <macspyrad@gmail.com>
2022-04-26 17:55:50 -0300

    implementing React Router on the project

---
+++ crwn-clothing/src/components/routes/navigation/navigation.component.jsx
@@ -0,0 +1,23 @@
+import { Outlet, Link } from "react-router-dom"
+import "./navigation.styles.scss"
+
+import { ReactComponent as CrwnLogo} from "../../../assets/crown.svg"
+
+export const Navigation = () => {
+    return (
+      <>
+        <div className="navigation">
+          <Link className="logo-container" to="/"> 
+            <CrwnLogo className="logo" />
+          </Link>
+
+          <div className="nav-links-container">
+            <Link className="nav-link" to="/shop">
+                Shop
+            </Link>
+          </div>
+        </div>
+        <Outlet />
+      </>
+    )
+  }
