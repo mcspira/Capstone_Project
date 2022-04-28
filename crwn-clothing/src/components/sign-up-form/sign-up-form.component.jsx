96df4d73f8daa7fb829a51892fcb6750234058d5
mcspira <macspyrad@gmail.com>
2022-04-28 11:29:15 -0300

    Creating a Sass input component and make the form input component

--- crwn-clothing/src/components/sign-up-form/sign-up-form.component.jsx
+++ crwn-clothing/src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,76 +1,103 @@
-import { useState } from "react"
+import { useState } from "react";
 
-import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils"
+import { FormInput } from "../form-input/form-input.component";
+
+import {
+  createAuthUserWithEmailAndPassword,
+  createUserDocumentFromAuth,
+} from "../../utils/firebase/firebase.utils";
 
 const defaultFormFields = {
-  displayName: '',
-  email: '',
-  password: '',
-  confirmPassword: ''
-}
+  displayName: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
 export const SignUpForm = () => {
-  const [ formFields, setFormFields ] = useState(defaultFormFields)
-  const { displayName, email, password, confirmPassword } = formFields
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { displayName, email, password, confirmPassword } = formFields;
 
-  console.log(formFields)
+  console.log(formFields);
 
   const resetFormFields = () => {
-    setFormFields(defaultFormFields)
-  }
+    setFormFields(defaultFormFields);
+  };
   const handleSubmit = async (event) => {
-    event.preventDefault()
+    event.preventDefault();
 
-    if(password !== confirmPassword) {
-      alert("Passwords do not match")
-      return
+    if (password !== confirmPassword) {
+      alert("Passwords do not match");
+      return;
     }
 
     try {
-      const { user } = await createAuthUserWithEmailAndPassword(email, password)
+      const { user } = await createAuthUserWithEmailAndPassword(
+        email,
+        password
+      );
 
-      await createUserDocumentFromAuth(user, { displayName })
+      await createUserDocumentFromAuth(user, { displayName });
 
-      resetFormFields()
-    } catch (error){
-      if(error.code === "auth/weak-password") {
-        alert("The minimum lenght of the password is 6, plese try again!")
-      }
-      else if(error.code === "auth/email-already-in-use") {
-        alert("This email has already been used")
-      }
-
-      else {
-        console.log("User creation encountered an error", error)
+      resetFormFields();
+    } catch (error) {
+      if (error.code === "auth/weak-password") {
+        alert("The minimum lenght of the password is 6, plese try again!");
+      } else if (error.code === "auth/email-already-in-use") {
+        alert("This email has already been used");
+      } else {
+        console.log("User creation encountered an error", error);
       }
     }
-  }
+  };
 
   const handleChange = (event) => {
-    const { name, value } = event.target
+    const { name, value } = event.target;
 
-    setFormFields({...formFields, [name]: value})
-  }
-	return (
+    setFormFields({ ...formFields, [name]: value });
+  };
+
+  return (
     <div>
       <h1> Sign Up with your email and password</h1>
 
       <form onSubmit={handleSubmit}>
+        <FormInput
+          label="Display Name"
+          type="text"
+          required
+          onChange={handleChange}
+          name="displayName"
+          value={displayName}
+        />
+        <FormInput
+          label="Email"
+          type="email"
+          required
+          onChange={handleChange}
+          name="email"
+          value={email}
+        />
 
-        <label> Display Name</label>
-          <input type="text" required onChange={handleChange} name="displayName" value={displayName}/>
+        <FormInput
+          label="Password"
+          type="password"
+          required
+          onChange={handleChange}
+          name="password"
+          value={password}
+        />
 
-        <label> Email </label>
-          <input type="email" required onChange={handleChange} name="email" value={email}/>
-
-        <label> Password</label>
-          <input type="password" required onChange={handleChange} name="password" value={password}/>
-          
-        <label> Confirm Password</label>
-          <input type="password" required onChange={handleChange} name="confirmPassword"value={confirmPassword}/>  
+        <FormInput
+          label="Confirm Password"
+          type="password"
+          required
+          onChange={handleChange}
+          name="confirmPassword"
+          value={confirmPassword}
+        />
 
         <button type="submit"> Sign Up </button>
-
       </form>
     </div>
-  )
-}
+  );
+};
