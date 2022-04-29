252b5d9e386fd8ea0d40ebb591747a774453c7e6
mcspira <macspyrad@gmail.com>
2022-04-29 17:25:20 -0300

    Creating a context to sign in, and access a context in Navegation

--- crwn-clothing/src/routes/navigation/navigation.component.jsx
+++ crwn-clothing/src/routes/navigation/navigation.component.jsx
@@ -1,22 +1,33 @@
-import { Outlet, Link } from "react-router-dom"
-import "./navigation.styles.scss"
+import { useContext } from "react";
 
-import { ReactComponent as CrwnLogo} from "../../assets/crown.svg"
+import { Outlet, Link } from "react-router-dom";
+import "./navigation.styles.scss";
+
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+
+import { UserContext } from "../../contexts/user.context";
 
 export const Navigation = () => {
-    return (
-      <>
-        <div className="navigation">
-          <Link className="logo-container" to="/"> 
-            <CrwnLogo className="logo" />
-          </Link>
+  const { currentUser } = useContext(UserContext)
+  console.log(currentUser)
 
-          <div className="nav-links-container">
-            <Link className="nav-link" to="/shop">Shop</Link>
-            <Link className="signIn-link" to="/auth">Sign In</Link>
-          </div>
+  return (
+    <>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <CrwnLogo className="logo" />
+        </Link>
+
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            Shop
+          </Link>
+          <Link className="signIn-link" to="/auth">
+            Sign In
+          </Link>
         </div>
-        <Outlet />
-      </>
-    )
-  }
+      </div>
+      <Outlet />
+    </>
+  );
+};
