36593b3bac8093ce05a5e5275cbfe9f28ace822d
mcspira <macspyrad@gmail.com>
2022-05-04 17:41:24 -0300

    card item design and creating a count for the bag

--- crwn-clothing/src/components/card-icon/cart-icon.component.jsx
+++ crwn-clothing/src/components/card-icon/cart-icon.component.jsx
@@ -7,14 +7,14 @@ import { CartContext } from "../../contexts/cart.context";
 import "./cart-icon.styles.scss";
 
 export const CartIcon = () => {
-  const { isCartOpen, setIsCartOpen } = useContext(CartContext)
+  const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext)
 
   const toggleCartIsOpen = () => setIsCartOpen(!isCartOpen)
 
   return (
     <div className="cart-icon-container" onClick={toggleCartIsOpen}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="icon-count">0</span>
+      <span className="icon-count">{cartCount}</span>
     </div>
   );
 };
